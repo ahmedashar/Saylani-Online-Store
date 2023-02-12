8c89bf9e098692f322697ea6c4bfe010b0cf9102
Mairajk <ahmedashar12345@gmail.com>
2023-02-12 17:00:47 +0500

    add item

---
+++ src/components/BackNav/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './backNav.css'
+import { IconName } from "react-icons/md";
+
+export default function BackNav() {
+  return (
+    <div className='container back_nav_cont'>
+
+    </div>
+  )
+}
